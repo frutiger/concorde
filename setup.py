43f7b6e11b177fb0d059d01df7170170c2d813c2
Masud Rahman <mail@masudrahman.com>
2020-01-01 19:35:19 -0500

    setup.py: bump to 0.7.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='concorde',
-      version='0.6.8',
+      version='0.7.0',
       description='ACME client library, cli tool, and automation tool',
       url='https://github.com/frutiger/concorde',
       author='Masud Rahman',
